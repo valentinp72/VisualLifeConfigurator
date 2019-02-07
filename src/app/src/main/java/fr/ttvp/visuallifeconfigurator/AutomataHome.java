e4004eea8bd9099f2a899b9d39bde5df3383c505
Valentin Pelloin <valentinpelloin@gmail.com>
2019-02-07 10:59:31 +0100

    tabs are working

--- src/app/src/main/java/fr/ttvp/visuallifeconfigurator/AutomataHome.java
+++ src/app/src/main/java/fr/ttvp/visuallifeconfigurator/AutomataHome.java
@@ -1,5 +1,6 @@
 package fr.ttvp.visuallifeconfigurator;
 
+import android.annotation.SuppressLint;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,7 +20,7 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
-public class AutomataHomeActivity extends AppCompatActivity {
+public class AutomataHome extends AppCompatActivity {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -43,6 +44,7 @@ public class AutomataHomeActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -55,15 +57,6 @@ public class AutomataHomeActivity extends AppCompatActivity {
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
     }
 
@@ -82,46 +75,64 @@ public class AutomataHomeActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
-        }
+    public static class HomeTabCells extends Fragment {
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public HomeTabCells() {
             Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
+            this.setArguments(args);
+            System.out.println("creation");
+
+            /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });*/
         }
 
-        @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_automata_home, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
+            View view = inflater.inflate(R.layout.fragment_automata_home_cells, container, false);
+            System.out.println("oncreateview");
+
+            return view;
+        }
+    }
+
+    public static class HomeTabConfiguration extends Fragment {
+
+        public HomeTabConfiguration() {
+            Bundle args = new Bundle();
+            this.setArguments(args);
+        }
+
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_automata_home_configuration, container, false);
+
+            return view;
+        }
+    }
+
+
+    public static class HomeTabPlay extends Fragment {
+
+        public HomeTabPlay() {
+            Bundle args = new Bundle();
+            this.setArguments(args);
+        }
+
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_automata_home_play, container, false);
+
+            return view;
         }
     }
 
@@ -139,7 +150,15 @@ public class AutomataHomeActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            if(position == 0) {
+                return new HomeTabCells();
+            }
+            else if(position == 1) {
+                return new HomeTabConfiguration();
+            }
+            else {
+                return new HomeTabPlay();
+            }
         }
 
         @Override
